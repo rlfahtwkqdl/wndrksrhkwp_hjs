1deb4050a9cfbf4e380b52832e6769cb57772afe
rlfahtwkqdl <hongjunsu87@gmail.com>
2026-04-15 23:32:42 +0900

    타이틀 작업중

---
+++ wndrksrhkwp/Assets/Script/TitleManager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleManager : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject helpPanel;
+
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Stage1");
+    }
+
+    // Update is called once per frame
+    public void OpenHelp()
+    {
+        helpPanel.SetActive(true);
+    }
+
+    public void CloseHelp()
+    {
+        helpPanel.SetActive(false);
+    }
+
+    public void GameExit()
+    {
+        SceneManager.LoadScene("TitleScene");
+    }
+}
